8f6e0fe89900514c6fb447a200283656033799ee
Marko Trickovic <contact@markotrickovic.com>
2024-01-05 22:58:49 +0100

    chore Add 'hasMany' relationship in Order model

--- app/Models/OrderItem.php
+++ app/Models/OrderItem.php
@@ -5,12 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Order extends Model
+class OrderItem extends Model
 {
     use HasFactory;
-
-    public function orderItems()
-    {
-        return $this->hasMany(OrderItem::class);
-    }
 }
