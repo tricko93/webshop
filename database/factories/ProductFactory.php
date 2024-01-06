de0b7b33b6ec77f075e238aaffc72183b1739151
Marko Trickovic <contact@markotrickovic.com>
2024-01-06 23:04:02 +0100

    chore Create 'ProductFactory' factory

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Category;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Product>
+ */
+class ProductFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        // Get all category IDs from the database
+        $categoryIds = Category::pluck('id')->toArray();
+
+        return [
+            'name' => $this->faker->word,
+            'description' => $this->faker->paragraph,
+            'price' => $this->faker->randomFloat(2, 10, 1000),
+            'stock_quantity' => $this->faker->randomNumber(3),
+            'category_id' => $this->faker->randomElement($categoryIds)
+        ];
+    }
+}
