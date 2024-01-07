90f386c181938e53d410d7b7be3825ec0e3191be
Marko Trickovic <contact@markotrickovic.com>
2024-01-07 15:27:47 +0100

    chore Update AppServiceProvider with category var

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
+use App\Models\Category;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +21,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Share categories with all views
+        View::composer('*', function ($view) {
+            $view->with('categories', Category::take(7)->get());
+        });
     }
 }
