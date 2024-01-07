35bc0226beb05e4747f2c1d678a30cde5f877bb8
Marko Trickovic <contact@markotrickovic.com>
2024-01-07 22:08:27 +0100

    chore Create ProductController.php

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $products = Product::all();
+        return view('products.index', compact('products'));
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Product $product)
+    {
+        return view('products.show', compact('product'));
+    }
+}
