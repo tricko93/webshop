863466caa9e7a88d1bbcfa31b40769a20e742da6
Marko Trickovic <contact@markotrickovic.com>
2024-01-07 22:30:52 +0100

    chore Create TextInput component

---
+++ resources/views/components/text-input.blade.php
@@ -0,0 +1,15 @@
+<!-- resources/views/components/text-input.blade.php -->
+<div class="relative">
+    @if ('textarea' != $type)
+        <input x-ref="input-{{ $name }}" type="{{ $type }}"
+            placeholder="{{ $placeholder }}"
+            name="{{ $name }}" value="{{ old($name, $value) }}" id="{{ $name }}"
+            @class([
+                'w-full rounded-md border-0 py-1.5 px-2.5 text-sm ring-1 placeholder:text-slate-400 focus:ring-2',
+            ]) />
+    @else
+        <textarea id="{{ $name }}" name="{{ $name }}" @class([
+                'w-full rounded-md border-0 py-1.5 px-2.5 text-sm ring-1 placeholder:text-slate-400 focus:ring-2',
+        ])>{{ old($name, $value) }}</textarea>
+    @endif
+</div>
