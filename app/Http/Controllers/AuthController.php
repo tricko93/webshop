bf0715c1b907d3b1c8c49461e96bb06f8af13959
Marko Trickovic <contact@markotrickovic.com>
2024-01-07 22:08:02 +0100

    chore Create AuthController

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('auth.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
