ea35a488503149eee718ab635c31a33f326c90ca
Marko Trickovic <96424684+tricko93@users.noreply.github.com>
2024-01-15 13:43:10 +0100

    chore Create 'cart' table migration

---
+++ database/migrations/2024_01_15_104301_create_cart_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('cart', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id')->nullable();
+            $table->string('session_id')->nullable();
+            $table->unsignedBigInteger('product_id');
+            $table->string('product_name');
+            $table->decimal('price', 8, 2);
+            $table->integer('quantity');
+            $table->string('status')->default('active');
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
+            $table->unique(['user_id', 'product_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('cart');
+    }
+};
